1d9ea2506ef6079b4a55670c702c5aad630337fd
AdanG75 <ansg75@gmail.com>
2023-02-10 02:04:46 -0600

    Finish MovementOptionsDialogFragment.kt

--- app/src/main/java/com/example/fintech75/ui/fragments/MovementOptionsDialogFragment.kt
+++ app/src/main/java/com/example/fintech75/ui/fragments/MovementOptionsDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.example.fintech75.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.fintech75.R
 import com.example.fintech75.databinding.FragmentMovementOptionsDialogBinding
@@ -22,7 +24,94 @@ class MovementOptionsDialogFragment : DialogFragment(R.layout.fragment_movement_
     }
 
     private fun setup() {
-        TODO("Without destination fragments")
+        if (args.userType == "client") {
+            setStateWithdrawButton(false)
+            if (args.creditType == "local") {
+                setStateTransferButton(false)
+            } else {
+                setStateTransferButton(true)
+            }
+            setStatePayButton(true)
+            setStateDepositButton(true)
+        } else {
+            setStateWithdrawButton(true)
+            setStateTransferButton(true)
+            setStatePayButton(true)
+            setStateDepositButton(true)
+        }
+    }
+
+    private fun setStatePayButton(isEnable: Boolean = true) {
+        binding.bPay.isEnabled = isEnable
+        if (isEnable) {
+            binding.bPay.visibility = View.VISIBLE
+            binding.bPay.setOnClickListener {
+                Log.d(fragmentName, "Go to pay screen...")
+                val action = MovementOptionsDialogFragmentDirections
+                    .actionMovementOptionsDialogFragmentToPayFragment(
+                        currentCreditId = args.creditId,
+                        currentCreditType = args.creditType,
+                        marketId = "N/A"
+                    )
+                findNavController().navigate(action)
+            }
+        } else {
+            binding.bPay.visibility = View.GONE
+        }
+    }
+
+    private fun setStateDepositButton(isEnable: Boolean = true) {
+        binding.bDeposit.isEnabled = isEnable
+        if (isEnable) {
+            binding.bDeposit.visibility = View.VISIBLE
+            binding.bDeposit.setOnClickListener {
+                Log.d(fragmentName, "Go to deposit screen...")
+                val action = MovementOptionsDialogFragmentDirections
+                    .actionMovementOptionsDialogFragmentToDepositFragment(
+                        destinationCreditId = args.creditId,
+                        destinationCreditType = args.creditType
+                    )
+                findNavController().navigate(action)
+            }
+        } else {
+            binding.bDeposit.visibility = View.GONE
+        }
+    }
+
+    private fun setStateTransferButton(isEnable: Boolean = true) {
+        binding.bTransfer.isEnabled = isEnable
+        if (isEnable) {
+            binding.bTransfer.visibility = View.VISIBLE
+            binding.bTransfer.setOnClickListener {
+                Log.d(fragmentName, "Go to transfer screen...")
+                val action = MovementOptionsDialogFragmentDirections
+                    .actionMovementOptionsDialogFragmentToTransferFragment(
+                        originCreditId = args.creditId,
+                        originCreditType = args.creditType
+                    )
+                findNavController().navigate(action)
+            }
+        } else {
+            binding.bTransfer.visibility = View.GONE
+        }
+    }
+
+    private fun setStateWithdrawButton(isEnable: Boolean = true) {
+        binding.bWithdraw.isEnabled = isEnable
+        if (isEnable) {
+            binding.bWithdraw.visibility = View.VISIBLE
+            binding.bWithdraw.setOnClickListener {
+                Log.d(fragmentName, "Go to withdraw screen...")
+                val action = MovementOptionsDialogFragmentDirections
+                    .actionMovementOptionsDialogFragmentToWithdrawFragment(
+                        originCreditId = args.creditId,
+                        originCreditType = args.creditType
+                    )
+                findNavController().navigate(action)
+            }
+        } else {
+            binding.bWithdraw.visibility = View.GONE
+        }
     }
 
     override fun onStart() {
