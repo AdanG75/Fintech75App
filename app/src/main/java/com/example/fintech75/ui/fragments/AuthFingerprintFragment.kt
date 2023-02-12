9f7cbfc47f154a470365feed6f64b0f7dfe3e545
AdanG75 <ansg75@gmail.com>
2023-02-11 19:46:00 -0600

    Add Summary layout

---
+++ app/src/main/java/com/example/fintech75/ui/fragments/AuthFingerprintFragment.kt
@@ -0,0 +1,14 @@
+package com.example.fintech75.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.fintech75.R
+
+class AuthFingerprintFragment : Fragment(R.layout.fragment_auth_fingerprint) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
