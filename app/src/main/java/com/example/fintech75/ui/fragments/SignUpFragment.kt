a77c42e50b352a743234f7729ee96d9d177758a2
AdanG75 <ansg75@gmail.com>
2023-02-14 10:34:27 -0600

    Finish missing fragment layouts

--- app/src/main/java/com/example/fintech75/ui/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/fintech75/ui/fragments/SignUpFragment.kt
@@ -2,12 +2,19 @@ package com.example.fintech75.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.fintech75.R
+import com.example.fintech75.databinding.FragmentSignUpBinding
 import com.example.fintech75.ui.activities.MainActivity
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private val fragmentName = this::class.java.toString()
+
+    lateinit var binding: FragmentSignUpBinding
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (activity as MainActivity).hideBottomNavigation()
@@ -15,5 +22,23 @@ class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSignUpBinding.bind(view)
+
+        binding.bAddClient.setOnClickListener {
+            Log.d(fragmentName, "Go to sign-up client")
+
+            val action = SignUpFragmentDirections.actionSignUpFragmentToSignUpClientFragment()
+            findNavController().navigate(action)
+        }
+        binding.bAddClient.isEnabled = true
+
+        binding.bAddMarket.setOnClickListener {
+            Log.d(fragmentName, "Go to sign-up market")
+
+            val action = SignUpFragmentDirections.actionSignUpFragmentToSignUpMarketFragment()
+            findNavController().navigate(action)
+        }
+        binding.bAddMarket.isEnabled = true
     }
 }
